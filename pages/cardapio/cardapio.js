6e00d3e09f8916a07cacf3020e8cae3d5deca54e
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-10-30 22:37:14 -0300

    atualizando a pagina de cardapio

---
+++ pages/cardapio/cardapio.js
@@ -0,0 +1,11 @@
+$('.navbar2 a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('#menuh').innerHeight() + $('.navbar').innerHeight();
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+});
+
+    
