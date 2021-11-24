67252647f501b288713d7a5c0c9ccc9174ecc81f
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-11-24 13:01:57 -0300

    adicionando os documentos no modelo MVC e acessibilidade

--- visao/pages/sobre/sobre.js
+++ visao/pages/sobre/sobre.js
@@ -2,50 +2,50 @@ $(function(){
     $('#valor1').hide();
     $('#but1').click(function(){
         $('#valor1').toggle(500);
-        if($('#but1').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but1').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but1').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but1').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but1').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
     });
 
     $('#valor2').hide();
     $('#but2').click(function(){
         $('#valor2').toggle(500);
-        if($('#but2').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but2').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but2').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but2').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but2').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
     });
 
     $('#valor3').hide();
     $('#but3').click(function(){
         $('#valor3').toggle(500);
-        if($('#but3').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but3').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but3').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but3').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but3').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
     });
 
     $('#valor4').hide();
     $('#but4').click(function(){
         $('#valor4').toggle(500);
-        if($('#but4').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but4').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but4').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but4').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but4').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
     });
 
     $('#valor5').hide();
     $('#but5').click(function(){
         $('#valor5').toggle(500);
-        if($('#but5').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but5').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but5').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but5').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but5').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
     });
 });
--- visao/pages/unidades/unidades.js
+++ visao/pages/unidades/unidades.js
@@ -12,10 +12,10 @@ $(function(){
     $('#desc1').hide();
     $('#but1').click(function(){
         $('#desc1').toggle(500);
-        if($('#but1').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but1').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
+        if($('#but1').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
+            $('#but1').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>');
         }else{
-            $('#but1').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+            $('#but1').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>');
         }
         
     });
@@ -23,10 +23,10 @@ $(function(){
     $('#desc2').hide();
     $('#but2').click(function(){
         $('#desc2').toggle(500);
-        if($('#but2').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but2').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but2').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but2').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but2').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -34,10 +34,10 @@ $(function(){
     $('#desc3').hide();
     $('#but3').click(function(){
         $('#desc3').toggle(500);
-        if($('#but3').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but3').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but3').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but3').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but3').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -45,10 +45,10 @@ $(function(){
     $('#desc4').hide();
     $('#but4').click(function(){
         $('#desc4').toggle(500);
-        if($('#but4').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but4').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but4').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but4').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but4').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -56,10 +56,10 @@ $(function(){
     $('#desc5').hide();
     $('#but5').click(function(){
         $('#desc5').toggle(500);
-        if($('#but5').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but5').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but5').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but5').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but5').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -67,10 +67,10 @@ $(function(){
     $('#desc6').hide();
     $('#but6').click(function(){
         $('#desc6').toggle(500);
-        if($('#but6').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but6').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but6').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but6').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but6').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -78,10 +78,10 @@ $(function(){
     $('#desc7').hide();
     $('#but7').click(function(){
         $('#desc7').toggle(500);
-        if($('#but7').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but7').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but7').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but7').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but7').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -89,10 +89,10 @@ $(function(){
     $('#desc8').hide();
     $('#but8').click(function(){
         $('#desc8').toggle(500);
-        if($('#but8').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but8').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but8').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but8').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but8').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -100,10 +100,10 @@ $(function(){
     $('#desc9').hide();
     $('#but9').click(function(){
         $('#desc9').toggle(500);
-        if($('#but9').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but9').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but9').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but9').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but9').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -111,10 +111,10 @@ $(function(){
     $('#desc10').hide();
     $('#but10').click(function(){
         $('#desc10').toggle(500);
-        if($('#but10').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but10').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but10').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but10').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but10').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -122,10 +122,10 @@ $(function(){
     $('#desc11').hide();
     $('#but11').click(function(){
         $('#desc11').toggle(500);
-        if($('#but11').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but11').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but11').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but11').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but11').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
@@ -133,10 +133,10 @@ $(function(){
     $('#desc12').hide();
     $('#but12').click(function(){
         $('#desc12').toggle(500);
-        if($('#but12').html() == '<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>'){
-            $('#but12').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
-        }else{
+        if($('#but12').html() == '<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>'){
             $('#but12').html('<i class="fas fa-chevron-down" style="font-size: 25pt;"></i>')
+        }else{
+            $('#but12').html('<i class="fas fa-chevron-up" style="font-size: 25pt;"></i>')
         }
         
     });
