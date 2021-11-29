51bdea4b7599f2af4029f2d4decc7ae6d70c9aa4
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-11-29 16:31:31 -0300

    padronização do script

--- modelo/trabalheDAO.php
+++ modelo/trabalheDAO.php
@@ -5,9 +5,18 @@
                 $pdo = $conn;
                 $stmt = $pdo -> prepare("INSERT INTO trabalhe (nome, sobrenome, nascimento, celular, email, curriculo) VALUES (:nome, :sobrenome, :nascimento, :celular, :email, :curriculo)");
                 $stmt -> execute(array("nome" => "$nome", "sobrenome" => "$sobrenome", "nascimento" => "$nascimento", "celular" => "$celular", "email" => "$email", "curriculo" => "$curriculo"));
-                echo "<script>window.location = '../visao/pages/sobre/sobre.html'; alert('Formulário enviado com sucesso!'); </script>";
+                echo "
+                <script>
+                    window.location = '../visao/pages/sobre/sobre.html'; 
+                    alert('Formulário enviado com sucesso!'); 
+                </script>";
             }catch(PDOException $e){
-                echo "<script>window.location = '../visao/pages/contato/contato.html'; alert('Não foi possível enviar o formulário!'); console.log(" . $e -> getMessage() .") </script>";
+                echo "
+                <script>
+                    window.location = '../visao/pages/contato/contato.html'; 
+                    alert('Não foi possível enviar o formulário!'); 
+                    console.log(" . $e -> getMessage() .") 
+                </script>";
             }
 
         }
